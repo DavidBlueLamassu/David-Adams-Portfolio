8822991ba947878744b55d3540e1d36feda009f0
DavidBlueLamassu <dra21@cam.ac.uk>
2023-05-25 17:44:58 +0100

    Portfolio redone with Tailwind

--- src/components/pages/ProjectCard.js
+++ src/components/pages/ProjectCard.js
@@ -1,31 +1,5 @@
 import React from "react";
 
-const styles = {
-  // Centers each card body within its div
-  cardPosition: {
-    display: "flex",
-    justifyContent: "center"
-  },
-  // Formatting for each card, including font, background-color and size
-  card: {
-    background: "rgb(195, 219, 239)",
-    fontFamily: "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif",
-    height: "325px",
-    width: "325px",
-    boxShadow: "0 3px 6px #999, 0 3px 6px #999",
-    textAlign: "center",
-    marginBottom: "40px",
-  },
-  // Size formatting for each card image
-  image: {
-    height: "200px"
-  },
-  // Formatting for the links at the bottom of the card
-  links: {
-    textDecoration: "none",
-    color: "black",
-  },
-}
 //The props argument in the ProjectCard() function allows this component to be re-used repeatedly as a template in 
 //Portfolio.js where it takes in data stored as objects in projects.json. The HTML is a Bootstrap
 //card component: 'Images' from: https://getbootstrap.com/docs/4.3/components/card/ The images for 
@@ -35,18 +9,19 @@ const styles = {
 //https://levelup.gitconnected.com/display-images-in-react-8ff1f5b1cf9a and in Harshvardhan Singh Baghel's answer to a question
 //on "Stack Overflow": Zip, "How do I reference a local image in React?", Stack Overflow (12 October 2016), 
 //last viewed 14 March 2023: https://stackoverflow.com/questions/39999367/how-do-i-reference-a-local-image-in-react
+
 function ProjectCard(props) {
   return ( 
-    <article className="col-lg-4 col-md-6 col-sm-12" style={styles.cardPosition}> {/* Class name ensures that cards are responsive */}
-      <div className="card" style={styles.card}>
-        <img src={props.screenshot} className="card-img-top" alt={props.title} style={styles.image}/>
-        <div className="card-body">
-          <h5 className="card-text">{props.title}</h5>
+    <article className="w-80 h-80 shadow-2xl shadow-slate-800 rounded-md"> {/* Class name ensures that cards are responsive */}
+      <div className="">
+        <img src={props.screenshot} alt={props.title} className="h-48 w-80 rounded-t-md"/>
+        <div className="text-center pt-4">
+          <h2 className="text-lg">{props.title}</h2>
           <div>
-            <a href={props.url} style={styles.links}>URL</a>
+            <a href={props.url} className="text-sm hover:text-red-600">URL</a>
           </div>
           <div>
-            <a href={props.gitHub} style={styles.links}>GitHub</a>
+            <a href={props.gitHub} className="text-sm hover:text-red-600">GitHub</a>
           </div>
         </div>
       </div>
