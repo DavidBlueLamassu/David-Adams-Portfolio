2e2931520ed7825408cdaec6e4a754fbd61156e8
DavidBlueLamassu <dra21@cam.ac.uk>
2023-08-03 20:12:12 +0100

    Selected Navbar link becomes a darker red

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -13,9 +13,9 @@ function NavBar() {
 
   //useState variable to change the color of the most recently selected navbar link to red in order to indicate which page is currently displayed.
   const [pageState, setPageState] = useState({
-    homeText: "md:text-red-500",
-    portfolioText: "md:text-stone-500",
-    contactText: "md:text-stone-500"
+    homeText: "md:text-red-500 md:hover:text-red-600",
+    portfolioText: "md:text-stone-500 md:hover:text-stone-800",
+    contactText: "md:text-stone-500 md:hover:text-stone-800"
   });
 
   return (
@@ -34,19 +34,18 @@ function NavBar() {
       
       {/* When the value of showNav === true then the hamburger menu is displayed; when it is false the menu is not displayed (is
         displayed off screen). The remainder of the formatting controls how the hamburger menu and the navbar menu (for medium 
-        screens and up) are displayed. All menu items are set to change from the default color to black upon hover. The frosted 
-        navbar effect derives, in part, from code in Jon Catmull's answer to a query in "Stack Overlow": Chad Johnson, "How can I make a CSS glass/blur effect work for an overlay?", Stack Overflow (20 December 2014), last viewed 1 August 2023: 
+        screens and up) are displayed. The frosted navbar effect derives, in part, from code in Jon Catmull's answer to a query in "Stack Overlow": Chad Johnson, "How can I make a CSS glass/blur effect work for an overlay?", Stack Overflow (20 December 2014), last viewed 1 August 2023: 
         https://stackoverflow.com/questions/27583937/how-can-i-make-a-css-glass-blur-effect-work-for-an-overlay, and from a video
         by Saraev Media: "How to Create a Frosted Navbar with TailwindCSS", YouTube (2021), last viewed 1 August 2023: 
         https://www.youtube.com/watch?v=nK4J7CguMFc */}
       <ul className={(showNav ? "left-0" : "-left-full") + " fixed bg-stone-500 w-4/12 h-40 text-white space-y-5 md:space-y-0 p-5 md:static md:bg-stone-300 md:bg-opacity-0 md:backdrop-blur-sm md:flex md:h-14 font-serif md:items-center md:border-b-2 md:border-stone-500 text-sm md:w-full bg-opacity-90"}>
-        <li className={pageState.homeText + " md:pl-2 hover:text-stone-800"}>
+        <li className={pageState.homeText + " md:pl-2"}>
           
-          {/* The pageState, setPageState variable changes the color of the most recently clicked link to red, using onClick, in order to indicate which page is currently displayed. */}
+          {/* The pageState, setPageState variable changes the color of the most recently clicked link to red, using onClick, in order to indicate which page is currently displayed. Links become darker upon hover.*/}
           <NavLink onClick={() => setPageState(
-            {...pageState, homeText: "md:text-red-500",
-            portfolioText: "md:text-stone-500",
-            contactText: "md:text-stone-500"
+            {...pageState, homeText: "md:text-red-500 md:hover:text-red-600",
+            portfolioText: "md:text-stone-500 md:hover:text-stone-800",
+            contactText: "md:text-stone-500 md:hover:text-stone-800"
             }
             )}
             to="/"
@@ -55,11 +54,11 @@ function NavBar() {
             Home
           </NavLink>
         </li>
-        <li className={pageState.portfolioText + " md:pl-5 hover:text-stone-800"}>
+        <li className={pageState.portfolioText + " md:pl-5"}>
           <NavLink onClick={() => setPageState(
-            {...pageState, homeText: "md:text-stone-500",
-            portfolioText: "md:text-red-500",
-            contactText: "md:text-stone-500"
+            {...pageState, homeText: "md:text-stone-500 md:hover:text-stone-800",
+            portfolioText: "md:text-red-500 md:hover:text-red-600",
+            contactText: "md:text-stone-500 md:hover:text-stone-800"
             }
             )}
             to="portfolio"
@@ -67,11 +66,11 @@ function NavBar() {
             Portfolio
           </NavLink>
         </li>
-        <li className={pageState.contactText + " md:pl-5 hover:text-stone-800"}>
+        <li className={pageState.contactText + " md:pl-5"}>
           <NavLink onClick={() => setPageState(
-            {...pageState, homeText: "md:text-stone-500",
-            portfolioText: "md:text-stone-500",
-            contactText: "md:text-red-500"
+            {...pageState, homeText: "md:text-stone-500 md:hover:text-stone-800",
+            portfolioText: "md:text-stone-500 md:hover:text-stone-800",
+            contactText: "md:text-red-500 md:hover:text-red-600"
             }
             )}
             to="contact"
