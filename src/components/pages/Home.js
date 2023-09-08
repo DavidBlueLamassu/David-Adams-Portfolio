48d616e2134f5eaafcdd44c5fed896b0f99b6f32
DavidBlueLamassu <dra21@cam.ac.uk>
2023-09-08 18:07:23 +0100

    CV/biog updated

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -13,8 +13,7 @@ function Home() {
       <article className="flex flex-col md:flex-row items-center md:items-start">
         <img alt="David Adams" src="./../assets/images/portrait.jpg" className="w-40 md:mr-10 md:ml-20 pb-6 md:pt-4 lg:pt-0"></img>
         <p className="text-lg text-justify md:mr-20 mx-4 bg-blue-200">
-        Welcome to my portfolio. I am an aspiring web designer. At the end of March, I completed a 16-week coding bootcamp in Front 
-        End Web Development run by EdX, graduating with a mark of 98.5%. My skills include coding with: React.js, TailwindCSS, 
+        Welcome to my portfolio. I am a Junior Frontend Developer and I am excited to have just begun an internship at Caustaza Group Ltd. My skills include coding with: React.js, TailwindCSS, 
         Node.js, JavaScript/jQuery, Bootstrap, CSS and HTML. To date, I have deployed 12 webpages on GitHub pages and 3 on Netlify. 
         I also have 2 GitHub repos dedicated to backend applications using Node. I particularly enjoy the problem-solving aspects of 
         working with JavaScript and its offshoots. I am also interested in developing animations using Tailwind and CSS. Examples 
